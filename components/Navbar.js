c33aeeb0146076f1776235dfd2d16c8d179f65df
Alex Smith <smithavt14@gmail.com>
2022-10-29 19:23:15 +0800

    removed text from nav, added icon to hero

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,9 @@
 const Navbar = () => {
   return (
-    <div className="fixed inset-x-0 top-0 py-8 px-16 w-screen text-white flex justify-between items-center z-10 invisible sm:visible">
-      <a href="#" className="font-semibold transition hover:text-slate-400">Back to Top</a>
-      <div className="flex justify-between items-center">
-        <a href="#" className="font-semibold px-4 transition hover:animate-wiggle hover:text-slate-400">About</a>
-        <a href="#" className="font-semibold px-4 transition hover:text-slate-400">Work</a>
-        <a href="#" className="font-semibold pl-4 transition hover:text-slate-400">Contact</a>
-      </div>
+    <div className="fixed inset-x-0 top-0 py-8 px-16 w-screen text-white flex justify-end items-center z-10 invisible sm:visible">
+      <a href="#" className="font-semibold px-4 transition hover:text-slate-400">About</a>
+      <a href="#" className="font-semibold px-4 transition hover:text-slate-400">Work</a>
+      <a href="#" className="font-semibold pl-4 transition hover:text-slate-400">Contact</a>
     </div>
   );
 }
