8424344a7df4f51663e182e6878eb295f6036e30
Alex Smith <smithavt14@gmail.com>
2022-11-01 22:57:05 +0800

    move svg to component, style it, write bio for about

--- components/About.js
+++ components/About.js
@@ -2,27 +2,33 @@
 import Image from 'next/image'
 import Headshot from '../public/images/headshot.png'
 import HeadshotBg from '../public/images/headshot-bg.svg'
+import AboutSVG from './AboutSVG'
 
 const About = () => {
   return (
     <section id="about" className="snap-center min-h-screen flex items-center px-6 md:px-12 lg:px-24 xl:p48">
-      <div className="mx-auto max-w-4xl flex justify-between">
+      <div className="mx-auto max-w-5xl flex justify-between relative">
         <div className="w-1/2 mx-4">
           <div className="flex items-center justify-between">
             <h3 className="text-4xl font-bold text-deepPurple dark:text-white">About Me</h3>
-            <div className="ml-4 grow-1 h-px  w-6/12 bg-deepPurple dark:bg-white"></div>
+            <div className="mx-8 grow h-px  bg-deepPurple dark:bg-white"></div>
           </div>
           <div className="mt-4 text-black dark:text-slate-400">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure harum a ratione tempora labore? Quisquam suscipit quo impedit, dolores tenetur delectus. Officia eum natus tenetur qui temporibus molestias repellat</p>
-            <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure harum a ratione tempora labore? Quisquam suscipit quo impedit, dolores tenetur delectus. Officia eum natus tenetur qui temporibus molestias repellat a.</p>
+            <p className="mb-2">Why hello there! I&apos;m Alex and I enjoy creating meaningful experiences for people both on/offline.</p>
+
+            <p className="mb-2">Over the last eight years, I&apos;ve worked in China, helping build digital experiences for <strong className="text-deepPurple dark:text-white">multi-national companies, startups, individuals, and of course - myself.</strong></p>
+
+            <p className="mb-2">Most recently, I&apos;ve been teaching others how to pursue careers in Web & Data by operating <a className="text-deepPurple dark:text-white font-bold	" href="https://www.lewagon.com/shanghai">the World&apos;s #1 Ranked Coding Bootcamp</a> in the heart of Shanghai, China.</p>
+
+            <p className="mb-2">Apart from my interests in coding, design, and digital strategy, I spend an enormous amount of time on fitness, creating content, and hanging with my dog - Heizi. You can see more about my projects and experiences below.</p>
           </div>
         </div>
         <div className="relative w-1/2 mx-4 flex justify-center items-center">
           <div className="relative h-5/6 w-8/12 z-10">
-            <Image className="rounded-3xl" src={Headshot} layout="fill" objectFit="cover" />
+            <Image className="rounded-3xl" src={Headshot} objectPosition="center" layout="fill" objectFit="contain" />
           </div>
-          <Image className="z-0 dark:fill-orange-400" src={HeadshotBg} layout="fill" objectFit="contain" objectPosition="center" />
         </div>
+        <AboutSVG className="absolute z-0"/>
       </div>
     </section>
    );
---
+++ components/AboutSVG.js
@@ -0,0 +1,9 @@
+const AboutSVG = () => {
+  return (
+    <svg className="dark:fill-deepPurple/10 fill-sky-400/20 absolute scale-125 inset-y-10 right-8" width="434" height="354" viewBox="0 0 434 354" xmlns="http://www.w3.org/2000/svg">
+      <path d="M434 181.115C434 276.597 284.041 354 188.792 354C93.5421 354 44.2893 272.39 16.327 181.115C-1.1995 123.905 -9.31059 58.042 16.327 27.8668C47.9609 -9.36602 121.758 -1.85673 188.792 8.22993C304.568 25.6509 434 85.6332 434 181.115Z"/>
+    </svg>
+   );
+}
+
+export default AboutSVG;
