25ffb9ea35b146227a4967cf94488ca7daaef8c7
Alex Smith <smithavt14@gmail.com>
2022-11-29 16:06:10 +0800

    Change style of color toggle

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,20 +5,19 @@ import { BsFillSunFill } from "react-icons/bs";
 
 const Navbar = ({darkMode, toggleDarkMode}) => {
   return (
-    <div className="fixed inset-x-0 top-0 py-8 px-12 w-screen text-white flex justify-center md:justify-end items-center z-50 ">
-      <a href="#" className="font-semibold mr-8 transition hover:text-deepPurple">About</a>
-      <a href="#" className="font-semibold mr-8 transition hover:text-deepPurple">Work</a>
-      <a href="#" className="font-semibold mr-8 transition hover:text-deepPurple">Contact</a>
+    <div className="transition-all duration-200 fixed inset-x-0 top-0 py-8 px-12 w-screen text-midnight dark:text-white flex justify-center md:justify-end items-center z-50">
+      <a href="#" className="mr-8 duration-200 hover:text-slate-400">About</a>
+      <a href="#" className="mr-8 duration-200 hover:text-slate-400">Work</a>
+      <a href="#" className="mr-8 duration-200 hover:text-slate-400">Contact</a>
 
       <div onClick={toggleDarkMode} className="
-      group
-      px-6 py-2 mr-8
-      cursor-pointer border-2 rounded-full border-deepPurple dark:border-white dark:hover:border-deepPurple
-      bg-deepPurple dark:bg-transparent
-      flex items-center
-      transition-all duration-500">
-        <BsMoonFill className="transition-all duration-500 fill-white/20 group-hover:fill-white/80 dark:fill-white dark:focus:fill-white shrink-0 w-5 h-5"/>
-        <BsFillSunFill className="transition-all duration-500 ml-6 w-5 h-5 fill-white shrink-0 dark:fill-white/20 dark:group-hover:fill-white/80"/>
+      group min-h-7 py-2 px-4 cursor-pointer border-2 rounded-full flex items-center transition-all duration-200
+      border-midnight dark:border-white relative">
+        <div className="transition-all duration-200 h-7 w-7 rounded-full absolute
+        bg-midnight right-2
+        dark:bg-white dark:right-14 dark:mr-1"></div>
+        <BsMoonFill className="transition-all duration-200 fill-midnight shrink-0 w-5 h-5"/>
+        <BsFillSunFill className="transition-all duration-500 ml-6 w-5 h-5 fill-white shrink-0"/>
       </div>
     </div>
   );
