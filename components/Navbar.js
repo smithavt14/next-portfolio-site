6a0223886f0cd8584ee93a77321918f8af582e54
Alex Smith <smithavt14@gmail.com>
2023-08-20 20:31:26 +0800

    added social icons and fixed mobile design

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,22 +1,22 @@
 import { BsMoonFill } from 'react-icons/bs';
 import { BsFillSunFill } from "react-icons/bs";
 
-const Navbar = ({darkMode, toggleDarkMode}) => {
+const Navbar = ({ darkMode, toggleDarkMode }) => {
   const endAnimation = (event) => {
     event.target.classList.remove('opacity-0')
     event.target.classList.add('opacity-100')
   }
 
   return (
-    <div onAnimationEnd={(event) => endAnimation(event)} className="transition-all duration-200 fixed inset-x-0 top-0 py-8 px-12 w-screen text-midnight dark:text-white flex justify-center md:justify-end items-center z-50 opacity-0 animate-fadeUp" style={{animationDelay: '2s'}}>
+    <div onAnimationEnd={(event) => endAnimation(event)} className="transition-all duration-200 fixed inset-x-0 top-0 p-6 md:p-12 lg:px-24 w-screen text-midnight dark:text-white flex justify-end items-center z-50 opacity-0 animate-fadeUp" style={{ animationDelay: '2s' }}>
       <div onClick={toggleDarkMode} className="
       group min-h-7 py-2 px-4 cursor-pointer border-2 rounded-full flex items-center transition-all duration-200
       border-midnight dark:border-white relative">
         <div className="transition-all duration-200 h-7 w-7 rounded-full absolute
         bg-midnight right-2
         dark:bg-white dark:right-14 dark:mr-1"></div>
-        <BsMoonFill className="transition-all duration-200 fill-midnight shrink-0 w-5 h-5"/>
-        <BsFillSunFill className="transition-all duration-500 ml-6 w-5 h-5 fill-white shrink-0"/>
+        <BsMoonFill className="transition-all duration-200 fill-midnight shrink-0 w-5 h-5" />
+        <BsFillSunFill className="transition-all duration-500 ml-6 w-5 h-5 fill-white shrink-0" />
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,11 @@ import { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import PageHead from '../components/Head';
-import About from '../components/About';
-import Work from '../components/Work';
-import Footer from '../components/Footer';
 
 export default function Home() {
 
   const [darkMode, darkToggle] = useState(true);
   const toggleDarkMode = () => { darkToggle(!darkMode) };
-
-  const [workContent, workToggle] = useState(false);
-  const toggleWorkContent = () => { workToggle(!workContent) };
 
   return (
     <div className={`select-none ${darkMode ? 'dark bg-midnight' : 'bg:'}`}>
