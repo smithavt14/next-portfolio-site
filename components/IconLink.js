6afe335cd2afb91cb5a1a2f85f73cb93da97229e
Alex Smith <smithavt14@gmail.com>
2024-10-07 12:31:39 +0800

    Refactor IconLink component to add hover animation and improve accessibility

--- components/IconLink.js
+++ components/IconLink.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const IconLink = ({ href, target, rel, Icon, style }) => {
   return (
     <Link className="mr-4 cursor-pointer w-7 h-7" href={href} target={target} rel={rel} passHref>
-      <Icon className="w-full h-full opacity-0 animate-fadeUp fill-midnight dark:fill-white hover:fill-slate-400 dark:hover:fill-slate-400 transition" style={style} />
+      <Icon className="w-full h-full opacity-0 animate-fadeUp fill-midnight dark:fill-white hover:fill-slate-400 hover:-translate-y-1 dark:hover:fill-slate-400 transition" style={style} />
     </Link>
   );
 };
