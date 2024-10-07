360ab26541fe31a5a5e8c511eb362dad76414f63
Alex Smith <smithavt14@gmail.com>
2024-10-07 12:40:13 +0800

    Change x-padding on Navbar.

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = ({ darkMode, toggleDarkMode, animationDelay }) => {
 
   const justifyContent = pathname === '/blog' || pathname === '/blog/[slug]' ? 'space-between' : 'flex-end';
 
-  const renderBackButton = () => {    
+  const renderBackButton = () => {
     if (pathname !== '/') {
       return (
         <button
@@ -30,17 +30,20 @@ const Navbar = ({ darkMode, toggleDarkMode, animationDelay }) => {
   }
 
   return (
-    <nav onAnimationEnd={(event) => endAnimation(event)} style={{ justifyContent, animationDelay }} className="transition-all duration-200 fixed inset-x-0 top-0 p-6 md:p-12 lg:px-24 w-screen text-midnight dark:text-white flex items-center z-50 opacity-0 animate-fadeUp">
-      
+    <nav
+      onAnimationEnd={(event) => endAnimation(event)}
+      style={{ justifyContent, animationDelay }}
+      className="transition-all duration-200 w-screen fixed inset-x-0 top-0 p-6 md:p-12 lg:px-24 xl:px-48 text-midnight dark:text-white flex items-center z-50 opacity-0 animate-fadeUp">
+
       {renderBackButton()}
-      
+
       <div onClick={toggleDarkMode} className="group min-h-7 py-2 px-4 cursor-pointer border-2 rounded-full flex items-center transition-all duration-200 border-midnight dark:border-white relative">
-        
+
         <div className="transition-all duration-200 h-7 w-7 rounded-full absolute bg-midnight right-2 dark:bg-white dark:right-14 dark:mr-1" />
-        
+
         <BsMoonFill className="transition-all duration-200 fill-midnight shrink-0 w-5 h-5" />
         <BsFillSunFill className="transition-all duration-500 ml-6 w-5 h-5 fill-white shrink-0" />
-      
+
       </div>
     </nav>
   );
