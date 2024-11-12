fd3e0ed422ad70c14b9467fe244777210d412dcb
Alex Smith <smithavt14@gmail.com>
2024-11-12 13:08:47 +0800

    Add support for GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,47 @@
 import '../styles/globals.css';
 import Layout from '@components/layout';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-L03HCBHKHQ', {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <Layout>
-      {(layoutProps) => <Component {...pageProps} {...layoutProps} />}
-    </Layout>
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-L03HCBHKHQ"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L03HCBHKHQ');
+          `,
+        }}
+      />
+      <Layout>
+        {(layoutProps) => <Component {...pageProps} {...layoutProps} />}
+      </Layout>
+    </>
   );
 }
 
