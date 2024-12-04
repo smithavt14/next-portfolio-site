2ca2f4cf933e3a880c38964a76fcf37ec63ec6ec
Alex Smith <smithavt14@gmail.com>
2024-12-04 18:47:31 +0800

    Update and simplify layout

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,18 @@
 import PageHead from '@components/Head';
 import Navbar from '@components/Navbar';
+import Background from '@components/Background';
 import { useBackground } from '@hooks/useBackground';
 
 const Layout = ({ children }) => {
   const { gradient, isDark } = useBackground();
 
   return (
-    <div className={`min-h-screen flex flex-col justify-center select-none -z-10 ${gradient} ${isDark ? 'dark' : ''}`}>
+    <main className={`min-h-screen select-none -z-10 px-6 py-24 md:py-36 md:px-12 lg:px-24 xl:px-40 lg:py-36 ${gradient} ${isDark ? 'dark' : ''}`}>
       <PageHead />
       <Navbar />
-      <main>
-        {children}
-      </main>
-    </div>
+      <Background />
+      {children}
+    </main>
   );
 };
 
