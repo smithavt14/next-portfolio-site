d5b05b0cd74c607a5a349183373314950ff651f7
Alex Smith <smithavt14@gmail.com>
2024-12-07 12:37:00 +0800

    Update file naming:

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '@styles/globals.css';
 import Layout from '@components/Layout';
 import { BackgroundProvider } from '@hooks/useBackground';
 import GoogleAnalytics from '@components/GoogleAnalytics';
