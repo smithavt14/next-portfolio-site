afbbeb8c6f9864ceed9f111020b2c0696ca6ae9e
Alex Smith <smithavt14@gmail.com>
2024-12-12 08:57:15 +0800

    Add transition to hover classes in IconLink

--- components/IconLink.js
+++ components/IconLink.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const IconLink = ({ href, target, rel, Icon, style }) => {
   return (
     <Link className="icon-link mr-4 cursor-pointer w-7 h-7" href={href} target={target} rel={rel} passHref>
-      <Icon className="w-full h-full fill-midnight dark:fill-white hover:opacity-80 dark:hover:fill-slate-400" style={style} />
+      <Icon className="transition-all duration-300 w-full h-full fill-midnight dark:fill-white hover:opacity-80 dark:hover:fill-slate-400" style={style} />
     </Link>
   );
 };
