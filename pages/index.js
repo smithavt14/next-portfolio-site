9a04aebc06ba6b37d8de17a7c897abd898769af1
Alex Smith <smithavt14@gmail.com>
2024-12-13 20:13:57 +0800

    Refactor Background component in Home component and update Layout structure
    - Reintroduced Background component in the Home page to remove it from new root level pages.
    - Updated Background's container class to disable pointer events for improved interaction handling.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import IconLink from '@components/IconLink';
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+import Background from '@components/Background';
 
 export default function Home() {
   const sectionRef = useRef(null);
@@ -12,12 +13,12 @@ export default function Home() {
     // Get main section children and the icon container's children
     const iconContainer = section.querySelector('#icon-links');
     const elements = [...section.children, ...(iconContainer ? iconContainer.children : [])];
-    
+
     setIsLoading(false);
-    
-    gsap.set(elements, { 
+
+    gsap.set(elements, {
       opacity: 0,
-      y: 20 
+      y: 20
     });
 
     gsap.to(elements, {
@@ -31,28 +32,31 @@ export default function Home() {
   }, []);
 
   return (
-    <section
-      ref={sectionRef}
-      className={`relative h-full flex flex-col box-border z-20 space-y-5 xl:pt-10 ${ isLoading ? 'opacity-0' : '' }`}>
-      <p className="text-base text-midnight dark:text-white">
-        Hi, my name is
-      </p>
-      <div className="font-extrabold text-responsive">
-        <h2 className="text-midnight dark:text-white">
-          Alex Smith.
-        </h2>
-        <h2 className="text-slate-400 dark:text-slate-400">
-          I build experiences.
-        </h2>
-      </div>
-      <p className="w-full text-base text-midnight dark:text-white lg:w-8/12">
-        I&apos;m an endurance athlete and product guy who specializes in building digital experiences.
-      </p>
-      <div id="icon-links" className="flex justify-start items-center">
-        <IconLink href="https://www.linkedin.com/in/smithavt14/" target="_blank" rel="noreferrer" Icon={FaLinkedin} />
-        <IconLink href="https://github.com/smithavt14" target="_blank" rel="noreferrer" Icon={FaGithub} />
-        <IconLink href="mailto:hello@alex.cn.com" Icon={FaEnvelope} />
-      </div>
-    </section>
+    <>
+      <Background />
+      <section
+        ref={sectionRef}
+        className={`relative h-full flex flex-col box-border z-20 space-y-5 xl:pt-10 ${isLoading ? 'opacity-0' : ''}`}>
+        <p className="text-base text-midnight dark:text-white">
+          Hi, my name is
+        </p>
+        <div className="font-extrabold text-responsive">
+          <h2 className="text-midnight dark:text-white">
+            Alex Smith.
+          </h2>
+          <h2 className="text-slate-400 dark:text-slate-400">
+            I build experiences.
+          </h2>
+        </div>
+        <p className="w-full text-base text-midnight dark:text-white lg:w-8/12">
+          I&apos;m an endurance athlete and product guy who specializes in building digital experiences.
+        </p>
+        <div id="icon-links" className="flex justify-start items-center">
+          <IconLink href="https://www.linkedin.com/in/smithavt14/" target="_blank" rel="noreferrer" Icon={FaLinkedin} />
+          <IconLink href="https://github.com/smithavt14" target="_blank" rel="noreferrer" Icon={FaGithub} />
+          <IconLink href="mailto:hello@alex.cn.com" Icon={FaEnvelope} />
+        </div>
+      </section>
+    </>
   );
 }
